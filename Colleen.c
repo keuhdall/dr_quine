1cc610e98e131f376fbc620b58e34fb2a98197ec
keuhdall <lmarques@student.42.fr>
2017-08-31 21:29:13 +0200

    corrected Colleen

--- Colleen.c
+++ Colleen.c
@@ -8,4 +8,4 @@ int main(){
  b
 */
 ft_null();
-char*a="#include<stdio.h>%c/*%c a%c*/%cvoid ft_null(){};%cint main(){%c/*%c b%c*/%cft_null();%cchar*a=%c%s%c;printf(a,10,10,10,10,10,10,10,10,10,10,34,a,34);}";printf(a,10,10,10,10,10,10,10,10,10,10,34,a,34);}
+char*a="#include<stdio.h>%c/*%c a%c*/%cvoid ft_null(){};%cint main(){%c/*%c b%c*/%cft_null();%cchar*a=%c%s%c;printf(a,10,10,10,10,10,10,10,10,10,10,34,a,34,10);}%c";printf(a,10,10,10,10,10,10,10,10,10,10,34,a,34,10);}
