0b653aca519441576b911e5c1fc60be5ff96c222
keuhdall <lmarques@student.42.fr>
2017-08-31 23:39:32 +0200

    added Sully

---
+++ Sully.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(void){
+int i = 5;
+char*p;
+asprintf(&p, "./Sully_%d.c", i-1);
+FILE*f=fopen(p, "w+");
+char*a="#include<stdio.h>%c#include<stdlib.h>%cint main(void){%cint i = %d;%cchar*p;%casprintf(&p, %c./Sully_%cd.c%c, i-1);%cFILE*f=fopen(p, %cw+%c);%cchar*a=%c%s%c;%cif(i > 1){%cfprintf(f,a,10,10,10,i-1,10,10,34,37,34,10,34,34,10,34,a,34,10,10,10,10,10,10,34,37,37,34,10,34,37,34,10,10,10,10);%cfclose(f);%cchar*compile;%cchar*exec;%casprintf(&compile, %cgcc -Wall -Wextra -Werror Sully_%cd.c -o Sully_%cd%c, i-1, i-1);%casprintf(&exec, %c./Sully_%cd%c, i-1);%csystem(compile);%csystem(exec);%c}%c}";
+if(i > 1){
+fprintf(f,a,10,10,10,i-1,10,10,34,37,34,10,34,34,10,34,a,34,10,10,10,10,10,10,34,37,37,34,10,34,37,34,10,10,10,10);
+fclose(f);
+char*compile;
+char*exec;
+asprintf(&compile, "gcc -Wall -Wextra -Werror Sully_%d.c -o Sully_%d", i-1, i-1);
+asprintf(&exec, "./Sully_%d", i-1);
+system(compile);
+system(exec);
+}
+}
