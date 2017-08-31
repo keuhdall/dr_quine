75462a0469b4e1b1fd9c31dabef9c65e7db5b3bf
keuhdall <lmarques@student.42.fr>
2017-08-31 20:28:25 +0200

    added Colleen

---
+++ Colleen.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/*
+ a
+*/
+void ft_null(){};
+int main(){
+/*
+ b
+*/
+ft_null();
+char*a="#include<stdio.h>%c/*%c a%c*/%cvoid ft_null(){};%cint main(){%c/*%c b%c*/%cft_null();%cchar*a=%c%s%c;printf(a,10,10,10,10,10,10,10,10,10,10,34,a,34);}";printf(a,10,10,10,10,10,10,10,10,10,10,34,a,34);}
