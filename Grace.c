85baf14c3e1ec660734861d4c80ede1703c83c95
Léonard Marques <lmarques@student.42.fr>
2017-11-22 01:45:04 +0100

    corrected Grace.c

--- Grace.c
+++ Grace.c
@@ -1,6 +1,11 @@
 #include<stdio.h>
+#define ABC DEF
+#define GHI JKL
 #define MAIN(content) int main() { content }
 MAIN(
-FILE*f=fopen("./Grace_child.c", "w+");
-char*a="#include<stdio.h>%c#define MAIN(content) int main() { content }%cMAIN(%cFILE*f=fopen(%c./Grace_child.c%c, %cw+%c);%cchar*a=%c%s%c;%cfprintf(f,a,10,10,10,34,34,34,34,10,34,a,34,10,10);)%c";
-fprintf(f,a,10,10,10,34,34,34,34,10,34,a,34,10,10);)
+/*
+ a
+*/
+FILE*f=fopen("./Grace_kid.c", "w+");
+char*a="#include<stdio.h>%c#define ABC DEF%c#define GHI JKL%c#define MAIN(content) int main() { content }%cMAIN(%c/*%c a%c*/%cFILE*f=fopen(%c./Grace_kid.c%c, %cw+%c);%cchar*a=%c%s%c;%cfprintf(f,a,10,10,10,10,10,10,10,10,34,34,34,34,10,34,a,34,10,10);)%c";
+fprintf(f,a,10,10,10,10,10,10,10,10,34,34,34,34,10,34,a,34,10,10);)
