5721ead507283861a6e88b7aae5bedc7efff34aa
Léonard Marques <lmarques@student.42.fr>
2017-11-22 21:41:23 +0100

    corrected Sully.go

--- bonus/Sully/Sully.go
+++ bonus/Sully/Sully.go
@@ -9,12 +9,12 @@ import (
 
 func main() {
 	i := 5
-	if (i <= 0) {
+	if (i < 0) {
 		os.Exit(0)
 	}
-	mkdirCmd := exec.Command("mkdir", fmt.Sprintf("Sully_%d", i-1))
+	mkdirCmd := exec.Command("mkdir", fmt.Sprintf("Sully_%d", i))
 	mkdirCmd.Run()
-	f,err := os.Create(fmt.Sprintf("./Sully_%d/Sully_%d.go", i-1, i-1))
+	f,err := os.Create(fmt.Sprintf("./Sully_%d/Sully_%d.go", i, i))
 	if (err != nil) {
 		panic(err)
 	}
@@ -30,12 +30,12 @@ import (
 
 func main() {
 	i := %d
-	if (i <= 0) {
+	if (i < 0) {
 		os.Exit(0)
 	}
-	mkdirCmd := exec.Command("mkdir", fmt.Sprintf("Sully_%%d", i-1))
+	mkdirCmd := exec.Command("mkdir", fmt.Sprintf("Sully_%%d", i))
 	mkdirCmd.Run()
-	f,err := os.Create(fmt.Sprintf("./Sully_%%d/Sully_%%d.go", i-1, i-1))
+	f,err := os.Create(fmt.Sprintf("./Sully_%%d/Sully_%%d.go", i, i))
 	if (err != nil) {
 		panic(err)
 	}
@@ -43,11 +43,11 @@ func main() {
 	s := %s
 	fmt.Fprintf(w, s, i-1, "%c"+s+"%c", 96, 96, 10)
 	w.Flush()
-	runCmd := exec.Command("go", "run", fmt.Sprintf("Sully_%%d/Sully_%%d.go", i-1, i-1))
+	runCmd := exec.Command("go", "run", fmt.Sprintf("Sully_%%d/Sully_%%d.go", i, i))
 	runCmd.Run()
 }%c`
 	fmt.Fprintf(w, s, i-1, "`"+s+"`", 96, 96, 10)
 	w.Flush()
-	runCmd := exec.Command("go", "run", fmt.Sprintf("Sully_%d/Sully_%d.go", i-1, i-1))
+	runCmd := exec.Command("go", "run", fmt.Sprintf("Sully_%d/Sully_%d.go", i, i))
 	runCmd.Run()
 }
